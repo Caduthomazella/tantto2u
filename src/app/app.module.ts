cd85e109d59f392016869c7b9af1aa9fbc226c71
Carlos Eduardo Leal Thomazella <caduthomazella@hotmail.com>
2020-08-14 10:11:01 -0300

    first commit | add ngboostrap and routes pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DeliveryComponent } from './pages/delivery/delivery.component';
+import { FranchiseeComponent } from './pages/franchisee/franchisee.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DeliveryComponent,
+    FranchiseeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
